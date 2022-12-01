014efe78fcd9df993449d5fd8ac098773702fc18
Hanna Lembak <lembakhanna@gmail.com>
2022-12-01 13:35:49 +0200

    Define new methods for init and destroy - than configure their names in the Spring config file

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -21,4 +21,15 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it "+ fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method  doMyStartupStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuffYoYo() {
+        System.out.println("TrackCoach: inside method  doMyCleanupStuffYoYo");
+    }
+
 }
