45c1c46f4516d545f0ae233e72910533bdf6791d
Hanna Lembak <lembakhanna@gmail.com>
2023-06-12 19:12:19 +0300

    Make execution match method in the package com.luv2code.springdemo

--- src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* add*(..))")
+    @Before("execution(* com.luv2code.springdemo.*.*(..))")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=========>>>> Executing @Before advice on addAccount");
     }
