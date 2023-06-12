8c418fc09eb1b3ec1e0b2d3ec7b7898759a59e7e
Hanna Lembak <lembakhanna@gmail.com>
2023-06-12 18:50:05 +0300

    Make execution match method named "addAccount" only in AccountDAO.class

--- src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void  com.luv2code.springdemo.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=========>>>> Executing @Before advice on addAccount");
     }
