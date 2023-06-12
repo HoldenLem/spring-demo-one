e176c0408832c0a6c237eca877afbeb987816756
Hanna Lembak <lembakhanna@gmail.com>
2023-06-12 19:10:13 +0300

    Make execution match method starting with "add" and with or any number of parameters

--- src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* add*(com.luv2code.springdemo.Account,..))")
+    @Before("execution(* add*(..))")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=========>>>> Executing @Before advice on addAccount");
     }
