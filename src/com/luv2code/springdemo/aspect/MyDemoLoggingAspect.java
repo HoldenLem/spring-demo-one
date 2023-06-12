957e54cf106732e9bc62fd56244a6ae6a36e6b52
Hanna Lembak <lembakhanna@gmail.com>
2023-06-12 19:58:14 +0300

    Make execution match method in the package com.luv2code.springdemo with Pointcut expression

--- src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/MyDemoLoggingAspect.java
@@ -2,14 +2,25 @@ package com.luv2code.springdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* com.luv2code.springdemo.*.*(..))")
+    @Pointcut("execution(* com.luv2code.springdemo.*.*(..))")
+    public void forDaoPackage(){
+
+    }
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n=========>>>> Executing @Before advice on addAccount");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics(){
+        System.out.println("\n ========>>> Perform api analytics");
     }
 }
