deb04b5ad9b85680710b2d669933ed4b10e94b63
Hanna Lembak <lembakhanna@gmail.com>
2023-06-13 10:28:19 +0300

    ordering aspects provided

--- src/com/luv2code/springdemo/aspect/BeforeAddAccountAdvice.java
+++ src/com/luv2code/springdemo/aspect/BeforeAddAccountAdvice.java
@@ -1,9 +1,13 @@
 package com.luv2code.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -11,7 +15,19 @@ import org.springframework.stereotype.Component;
 public class BeforeAddAccountAdvice {
 
     @Before("com.luv2code.springdemo.aspect.LuvAopExpression.forDAOPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=========>>>> Executing @Before advice on addAccount");
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
+
+        // display method signature
+        MethodSignature methodSignature =(MethodSignature) joinPoint.getSignature();
+        System.out.println("\n=========>>>> Method: " + methodSignature);
+
+        //display methods args
+
+        Object[] args = joinPoint.getArgs();
+
+        for(Object tempArgs: args){
+            System.out.println(tempArgs);
+        }
     }
+
 }
